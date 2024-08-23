d4c9902f15cc904a1e5455df913956c5ef57068b
PetrKavan31 <petrkavan31@gmail.com>
2024-08-23 07:41:20 +0200

    v2 - 2x while => for, position numer if statements => // 3 and % 3

--- tic_tac_toe_v2.py
+++ tic_tac_toe_v2.py
@@ -9,14 +9,12 @@ board = [
 ] # Model
 
 def print_board(): # View
-    i = 0
-    while i < len(board):
-        print("".join(board[i]))
-        i+=1
-
+    for i in board:
+        print("".join(i))
+    
 def player_move(): # Controller and View
     count = 1
-    while count < 10:    
+    for count in range(9):    
         player = 'X' if count % 2 != 0 else 'O'
         position = int(input(f"Player {player} turn. Enter the position (1-9): "))
         if position > 9 or position < 1:
@@ -28,25 +26,8 @@ def player_move(): # Controller and View
     print("End of game.")
 
 def position_number(position, player): # View
-    if position == 1:
-        board[0][0] = player
-    elif position == 2:
-        board[0][1] = player
-    elif position == 3:
-        board[0][2] = player
-    elif position == 4:
-        board[1][0] = player
-    elif position == 5:
-        board[1][1] = player
-    elif position == 6:
-        board[1][2] = player
-    elif position == 7:
-        board[2][0] = player
-    elif position == 8:
-        board[2][1] = player
-    elif position == 9:
-        board[2][2] = player
-
+    board[(position - 1) // 3][(position - 1) % 3] = player
+    
 def play_game(): # View
     print_board()
     player_move()
