9b804dcecab7c94694ed594d1d9744ca652c4782
PetrKavan31 <petrkavan31@gmail.com>
2024-09-04 16:43:38 +0200

    self edit

--- ttt_oop.py
+++ ttt_oop.py
@@ -7,33 +7,33 @@ class TicTacToe:
         # TODO - proper game logic
         return self.board.count('.') == 0
 
-    def make_move(self, player_on_turn, board):
-        while not game.is_over():
+    def make_move(self):
+        while not self.is_over():
             position = self.ask_player_position()
             if position not in range(1,10):
                 print("Wrong number.")
                 continue
-            if board[position - 1] == '.':
-                board[position - 1] = player_on_turn
+            if self.board[position - 1] == '.':
+                self.board[position - 1] = self.player_on_turn
             else: 
                 print("The position is covered, make a move to another position.")
                 continue
-            player_on_turn = 'X' if player_on_turn == 'O' else 'O'
-            game.print_board(game.board)
+            self.player_on_turn = 'X' if self.player_on_turn == 'O' else 'O'
+            self.print_board()
         print("End of game.")
 
-    def print_board(self, board):
+    def print_board(self):
         for i in range(0,9,3):
-            print("".join(board[i:(i+3)]))
+            print("".join(self.board[i:(i+3)]))
 
     def ask_player_position(self):
-        player_on_turn = game.player_on_turn
+        player_on_turn = self.player_on_turn
         return int(input(f"Player {player_on_turn} turn. Enter the position (1-9): "))
 
-game = TicTacToe()
+def main():
+    game = TicTacToe()
+    game.print_board()
+    game.make_move()
 
-draw_board = game.board
-game.print_board(draw_board)
-
-play = game.player_on_turn
-game.make_move(play, draw_board)
+if __name__ == "__main__":
+    main()
